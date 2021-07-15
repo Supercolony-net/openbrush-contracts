1686320ccd2a4605ec11a4a8ca03e7ff6d003acc
xgreenx <xgreenx9999@gmail.com>
2021-07-15 22:33:28 +0300

    Redesigned modifiers. Instead of copy/pasting the code from modifier declaration. We will directly call the modifier's function on rust level. First argument is instance of contract. Second argument is body code of function marked with modifier. Every next arguments can by specified by developer and past inside of modifier.

--- contracts/brush/proc_macros/modifiers.rs
+++ contracts/brush/proc_macros/modifiers.rs
@@ -1,34 +1,45 @@
 use quote::{
     quote,
+    quote_spanned,
     format_ident,
     ToTokens,
 };
 use syn::{
     ImplItemMethod,
+    parse_macro_input,
+    spanned::Spanned,
 };
 use proc_macro::{TokenStream};
 use proc_macro2::{
     TokenStream as TokenStream2,
     TokenTree,
 };
-use crate::metadata;
-use crate::internal::is_attr;
+
+const INSTANCE: &'static str = "__brush_instance_modifier";
 
 pub(crate) fn generate(_attrs: TokenStream, _input: TokenStream) -> TokenStream {
-    let attrs: TokenStream2 = _attrs.into();
     let input: TokenStream2 = _input.clone().into();
-    let modifiers: Vec<_> = attrs
-        .into_iter()
-        .filter_map(|token|
-            if let TokenTree::Ident(ident) = token {
-                Some(ident)
-            } else {
-                None
-            })
-        .collect();
 
+    let modifiers = parse_macro_input!(_attrs as syn::AttributeArgs);
     let mut impl_item = syn::parse2::<ImplItemMethod>(_input.into())
         .expect("Can't parse input of `modifiers` macro like a method.");
+
+    if impl_item.sig.inputs.is_empty() {
+        return (quote_spanned! {
+            impl_item.sig.inputs.span() =>
+                compile_error!("Modifiers can by applied only to method with first `self` argument.");
+        }).into();
+    }
+
+    let receiver;
+    if let syn::FnArg::Receiver(rec) = impl_item.sig.inputs.first().unwrap() {
+        receiver = rec;
+    } else {
+        return (quote_spanned! {
+            impl_item.sig.inputs.first().unwrap().span() =>
+                compile_error!("First argument in modifiers must be `self`.");
+        }).into();
+    }
 
     // We skip every function without body(it means that it contains only `{ ; }`)
     if impl_item.block.to_token_stream().to_string() == "{ ; }" {
@@ -38,39 +49,15 @@ pub(crate) fn generate(_attrs: TokenStream, _input: TokenStream) -> TokenStream 
         return code.into();
     }
 
-    let locked_file = metadata::get_locked_file();
-    let metadata = metadata::Metadata::load(&locked_file);
-    metadata.save_and_unlock(locked_file);
+    let mut block = impl_item.block.clone();
+    let mut body_index = 0;
 
-    let local_function_ident = format_ident!("{}_local", impl_item.sig.ident);
-    let block = impl_item.block.clone();
-
-    // Put the body of original function to local lambda function
-    let mut final_stmts = syn::parse2::<syn::Block>(quote! {
-        {
-            let mut #local_function_ident = || #block;
+    let modifiers: Vec<_> = modifiers.into_iter().filter_map(|nested_meta| {
+        match nested_meta {
+            syn::NestedMeta::Meta(meta) => Some(meta),
+            _ => None,
         }
-    }).unwrap().stmts;
-
-    let has_return = impl_item.sig.output != syn::ReturnType::Default;
-
-    let return_ident = format_ident!("{}_out", local_function_ident);
-    let function_call;
-
-    if has_return {
-        function_call = syn::parse2::<syn::Block>(quote! {
-            {
-                let #return_ident = #local_function_ident();
-            }
-        }).unwrap().stmts;
-    } else {
-        function_call = syn::parse2::<syn::Block>(quote! {
-            {
-                #local_function_ident();
-            }
-        }).unwrap().stmts;
-    }
-    final_stmts.extend(function_call);
+    }).collect();
 
     // Code of each modifier must be added in reverse order
     // Code of first modifier {
@@ -80,31 +67,52 @@ pub(crate) fn generate(_attrs: TokenStream, _input: TokenStream) -> TokenStream 
     //          }
     //      }
     // }
-    for modifier_ident in  modifiers.into_iter().rev() {
-        let modifier = metadata.modifiers.get(&modifier_ident.to_string());
+    for modifier_meta in  modifiers.into_iter().rev() {
+        // Replace every `self` with instance variable
+        block = replace_self(block);
 
-        let mut local_stmts = vec![];
-        for stmt in modifier.block.stmts.clone() {
-            if is_body_stmt(&stmt) {
-                // We found #[body]() statement, so we put body here
-                local_stmts.extend(final_stmts.clone());
-                continue;
-            }
-            local_stmts.push(stmt.clone());
+        // Put the body of original function to local lambda function
+        let (final_block, body_ident) = put_into_closure(receiver, block, body_index);
+        body_index += 1;
+
+        // It means modifiers without arguments, we can call path method directly.
+        if let syn::Meta::Path(method) = modifier_meta {
+            let stmts = final_block.stmts;
+            block = syn::parse2::<syn::Block>(quote! {
+                {
+                    #(#stmts)*
+                    #method(self, #body_ident)
+                }
+            }).unwrap();
+        } else if let syn::Meta::List(meta_list) = modifier_meta {
+            let method = meta_list.path;
+            let mut cloned_variables_idents = vec![];
+            let cloned_variables_definitions = meta_list.nested.iter()
+                .map(|nested_meta| {
+                    let cloned_ident = format_ident!("__brush_cloned_{}", cloned_variables_idents.len());
+                    cloned_variables_idents.push(cloned_ident.clone());
+                    quote! {
+                        let #cloned_ident = #nested_meta.clone();
+                    }
+                });
+
+            let stmts = final_block.stmts;
+            block = syn::parse2::<syn::Block>(quote! {
+                {
+                    #(#cloned_variables_definitions)*
+                    #(#stmts)*
+                    #method(self, #body_ident #(, #cloned_variables_idents )*)
+                }
+            }).unwrap();
+        } else {
+            return (quote_spanned! {
+                modifier_meta.span() =>
+                    compile_error!("Modifiers doesn't support MetaNameValue in arguments");
+            }).into();
         }
-        final_stmts = local_stmts;
     }
 
-    if has_return {
-        let return_value = syn::parse2::<syn::Block>(quote! {
-            {
-                return #return_ident;
-            }
-        }).unwrap().stmts;
-        final_stmts.extend(return_value);
-    }
-
-    impl_item.block.stmts = final_stmts;
+    impl_item.block = block;
 
     let code = quote! {
         #[cfg(not(feature = "ink-as-dependency"))]
@@ -117,18 +125,48 @@ pub(crate) fn generate(_attrs: TokenStream, _input: TokenStream) -> TokenStream 
     code.into()
 }
 
-fn is_body_stmt(stmt: &syn::Stmt) -> bool {
-    match stmt {
-        syn::Stmt::Semi(expr, _) =>
-            match expr {
-                syn::Expr::Tuple(tuple) => is_attr(&tuple.attrs, "body"),
-                _ => false,
+fn replace_self(block: syn::Block) -> syn::Block {
+    syn::parse2::<syn::Block>(recursive_replace_self(block.to_token_stream())).unwrap()
+}
+
+fn recursive_replace_self(token_stream: TokenStream2) -> TokenStream2 {
+    token_stream.into_iter()
+        .map(|token| {
+            match &token {
+                TokenTree::Ident(ident) =>
+                    if ident.to_string() == "self" {
+                        TokenTree::Ident(format_ident!("{}", INSTANCE))
+                    } else {
+                        token
+                    },
+                TokenTree::Group(group) => {
+                    TokenTree::Group(
+                        proc_macro2::Group::new(
+                            group.delimiter(),
+                            recursive_replace_self(group.stream())
+                        )
+                    )
+                }
+                _ => token,
             }
-        syn::Stmt::Expr(expr) =>
-            match expr {
-                syn::Expr::Tuple(tuple) => is_attr(&tuple.attrs, "body"),
-                _ => false,
-            }
-        _ => false,
-    }
+        }).collect()
+}
+
+fn put_into_closure(receiver: &syn::Receiver, block: syn::Block, index: u8) -> (syn::Block, syn::Ident) {
+    let body_ident = format_ident!("__brush_body_{}", index);
+    let instance_ident = format_ident!("{}", INSTANCE);
+
+    let reference = match receiver.mutability.is_some() {
+        true => quote! { &mut },
+        false => quote! { & },
+    };
+
+    // Put the body of original function to local lambda function
+    let final_block = syn::parse2::<syn::Block>(quote! {
+        {
+            let mut #body_ident = |#instance_ident: #reference Self| #block;
+        }
+    }).unwrap();
+
+    (final_block, body_ident)
 }
