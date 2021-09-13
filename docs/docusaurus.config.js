ab98772c0afe21aa52e34b6966036fb3d6002884
sven <semirazov94@gmail.com>
2021-09-13 20:07:41 +0300

    fix project name

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.svg',
   organizationName: 'supercolony-net',
-  projectName: 'openbrush-contracts',
+  projectName: 'docs.openbrush.io',
   themeConfig: {
     navbar: {
       logo: {
