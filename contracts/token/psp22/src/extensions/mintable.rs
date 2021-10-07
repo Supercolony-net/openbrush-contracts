cac7492b45a99693467f40426a24a826ee8b1e61
YaremenkoM <mariayaremenko13@gmail.com>
2021-10-07 15:34:49 +0300

    fix mintable extension description

--- contracts/token/psp22/src/extensions/mintable.rs
+++ contracts/token/psp22/src/extensions/mintable.rs
@@ -1,7 +1,5 @@
 /// Extension of [`PSP22`] that allows create `amount` tokens
 /// and assigns them to `account`, increasing the total supply
-/// tokens and those that they have an allowance for, in a way that can be
-/// recognized off-chain (via event analysis).
 use crate::traits::*;
 use brush::traits::{
     AccountId,
