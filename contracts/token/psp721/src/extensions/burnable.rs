c634e671d461b9f9731212692588a984b4a5300a
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-10-12 12:14:28 +0200

    added burn from to psp721

--- contracts/token/psp721/src/extensions/burnable.rs
+++ contracts/token/psp721/src/extensions/burnable.rs
@@ -1,6 +1,11 @@
 /// Extension of [`PSP721`] that allows token holders to destroy their tokens
 use crate::traits::*;
 
+use brush::traits::{
+    AccountId,
+    ZERO_ADDRESS,
+};
+
 #[brush::trait_definition]
 pub trait PSP721Burnable: IPSP721 {
     /// Destroys token with id equal to 'id'.
@@ -9,5 +14,27 @@ pub trait PSP721Burnable: IPSP721 {
     #[ink(message)]
     fn burn(&mut self, id: Id) {
         self._burn(id);
+    }
+
+    /// Destroys token with id equal to 'id' from 'account'
+    ///
+    /// caller must be approved to transfer tokens from 'account'
+    /// or to transfer token with 'id'
+    #[ink(message)]
+    fn burn_from(&mut self, account: AccountId, id: Id) {
+        let caller = Self::env().caller();
+
+        let is_approved = self.get_approved(id).unwrap_or(ZERO_ADDRESS.into()) == caller;
+        let is_approved_for_all = self.is_approved_for_all(account, caller);
+
+        assert_eq!(
+            is_approved || is_approved_for_all,
+            true,
+            "{}",
+            PSP721Error::NotApproved.as_ref()
+        );
+
+        self._remove_from(account, id.clone());
+        self._emit_transfer_event(account, ZERO_ADDRESS.into(), id);
     }
 }
