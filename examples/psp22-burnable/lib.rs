b2c0b3ccfabddbb7a5830039dddc2c13bbd33d59
YaremenkoM <mariayaremenko13@gmail.com>
2021-10-12 14:27:12 +0300

    change function name

--- examples/psp22-burnable/lib.rs
+++ examples/psp22-burnable/lib.rs
@@ -27,7 +27,7 @@ pub mod my_psp22 {
             instance
         }
 
-        fn multi_burn_from(&mut self, accounts: Vec<(AccountId, Balance)>) {
+        fn burn_from_many(&mut self, accounts: Vec<(AccountId, Balance)>) {
             for account in accounts.iter() {
                 self.burn_from(account.0, account.1);
             }
