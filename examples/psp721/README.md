e9010cf6c4e3daf0d4454afe659256cc5325dc7b
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-10-15 08:46:57 +0200

    added psp 721 example

---
+++ examples/psp721/README.md
@@ -0,0 +1,6 @@
+## PSP721 contract (ERC721 analogue)
+
+Implementation of [EIP-721](https://eips.ethereum.org/EIPS/eip-721) token standard in Polkadot blockchain.
+
+// TODO
+[See example](https://supercolony-net.github.io/openbrush-contracts/smart-contracts/psp22)
---
+++ examples/psp721/lib.rs
@@ -0,0 +1,44 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[brush::contract]
+pub mod my_psp721 {
+    use psp721::{
+        extensions::{
+            burnable::*,
+            mintable::*,
+        },
+        traits::*,
+    };
+
+    #[derive(Default, PSP721Storage, PSP721MetadataStorage)]
+    #[ink(storage)]
+    pub struct MyPSP721 {
+        #[PSP721StorageField]
+        psp721: PSP721Data,
+        #[PSP721MetadataStorageField]
+        metadata: PSP721MetadataData,
+        next_id: u8,
+    }
+
+    impl PSP721 for MyPSP721 {}
+
+    impl PSP721Burnable for MyPSP721 {}
+
+    impl PSP721Mintable for MyPSP721 {}
+
+    impl MyPSP721 {
+        /// A constructor which mints the first token to the owner
+        #[ink(constructor)]
+        pub fn new(_total_supply: Balance) -> Self {
+            let mut instance = Self::default();
+            instance.mint_token();
+            instance
+        }
+
+        /// Mint method which mints a token and updates the id of next token
+        fn mint_token(&mut self) {
+            self.mint([self.next_id; 32]);
+            self.next_id = self.next_id + 1;
+        }
+    }
+}
