74b98c4661a82340fc9ac26b09f4015d11fd6551
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-11-02 09:38:43 +0100

    comments to test

--- tests/psp22wrapper.tests.ts
+++ tests/psp22wrapper.tests.ts
@@ -39,39 +39,45 @@ describe('MY_PSP22_WRAPPER', () => {
         const { contract: wrapper, query: wQuery } = contracts['wrapper']
         const { query: uQuery, defaultSigner: sender } = contracts['psp22']
 
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
-
+        
+        // sender deposits 100 tokens
         await expect(fromSigner(wrapper, sender.address).tx.depositFor(sender.address, 100)).to.eventually.be.rejected
-
+        
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
     })
-
+    
     it('Deposit without underlying tokens should fail', async () => {
         const contracts = await setup()
         const { contract: wrapper, query: wQuery } = contracts['wrapper']
         const { contract: underlying, query: uQuery, defaultSigner: sender } = contracts['psp22']
-
+        
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
-
+        
+        // sender approves contract to spend their tokens, sender deposits 1001 tokens
         await expect(underlying.tx.approve(wrapper.address, 100)).to.eventually.be.fulfilled
         await expect(fromSigner(wrapper, sender.address).tx.depositFor(sender.address, 1001)).to.eventually.be.rejected
-
+        
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
     })
-
+    
     it('Withdraw to works', async () => {
         const contracts = await setup()
         const { contract: wrapper, query: wQuery } = contracts['wrapper']
         const { contract: underlying, query: uQuery, defaultSigner: sender } = contracts['psp22']
-
+        
         // sender allows the contract to spend their tokens
         await expect(underlying.tx.approve(wrapper.address, 100)).to.eventually.be.fulfilled
         // sender wraps 100 tokens
@@ -82,10 +88,10 @@ describe('MY_PSP22_WRAPPER', () => {
         await expect(wQuery.balanceOf(sender.address)).to.have.output(100)
         // contract has 100 tokens
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(100)
-
+        
         // sender withdraws 100 tokens
         await expect(fromSigner(wrapper, sender.address).tx.withdrawTo(sender.address, 100)).to.eventually.be.fulfilled
-
+        
         // sender has 1000 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         // sender has 0 wrapped tokens
@@ -93,18 +99,21 @@ describe('MY_PSP22_WRAPPER', () => {
         // contract has 0 tokens
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
     })
-
+    
     it('Withdraw without deposit should fail', async () => {
         const contracts = await setup()
         const { contract: wrapper, query: wQuery } = contracts['wrapper']
         const { query: uQuery, defaultSigner: sender } = contracts['psp22']
-
+        
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
-
+        
+        // sender withdraws 100 tokens
         await expect(fromSigner(wrapper, sender.address).tx.withdrawTo(sender.address, 100)).to.eventually.be.rejected
-
+        
+        // sender has 1000 tokens and 0 wrapper tokens, contract has 0 tokens
         await expect(uQuery.balanceOf(sender.address)).to.have.output(1000)
         await expect(wQuery.balanceOf(sender.address)).to.have.output(0)
         await expect(uQuery.balanceOf(wrapper.address)).to.have.output(0)
