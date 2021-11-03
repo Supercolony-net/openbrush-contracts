b51fd590bc21b8228faec5a9a33347b82d0ac59f
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-11-03 23:32:28 +0100

    token timelock integration test

---
+++ tests/token_timelock.tests.ts
@@ -0,0 +1,81 @@
+import { expect, fromSigner, setupContract } from './helpers'
+import { network } from 'redspot'
+
+const { api } = network
+
+describe('TOKEN_TIMELOCK', () => {
+    async function setup() {
+        let psp22 = await setupContract('my_psp22', 'new', '1000', 'TOKEN', 'TKN', 2)
+        const beneficiary = psp22.defaultSigner
+        let releaseTime = (Date.now() / 1000) + (24 * 60 * 60)
+        let timelock = await setupContract('my_psp22_token_timelock', 'new', psp22.contract.address, beneficiary.address, releaseTime)
+        return { psp22, timelock, beneficiary, releaseTime }
+    }
+
+    it('New works', async () => {
+        const { psp22: psp22Container, timelock: timelockContainer, beneficiary, releaseTime } = await setup()
+        const { contract: psp22 } = psp22Container
+        const { query: timelockQuery } = timelockContainer
+
+        await expect(timelockQuery.token()).to.have.output(psp22.address)
+        await expect(timelockQuery.beneficiary()).to.have.output(beneficiary.address)
+        await expect(timelockQuery.releaseTime()).to.have.output(releaseTime)
+    })
+
+    it('Release works', async () => {
+        const { psp22: psp22Container, timelock: timelockContainer, beneficiary, releaseTime } = await setup()
+        const { contract: psp22, query: psp22Query } = psp22Container
+        const { contract: timelock } = timelockContainer
+
+        let depositedTokens = 1000
+        // deposit tokens to the contract
+        await expect(fromSigner(psp22, beneficiary.address).tx.transfer(timelock.address, depositedTokens, [])).to.eventually.be.fulfilled
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(depositedTokens)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(0)
+        await api.tx.timestamp.set(releaseTime).signAndSend(beneficiary.pair)
+
+        // release the tokens
+        await expect(fromSigner(timelock, beneficiary.address).tx.release()).to.eventually.be.fulfilled
+
+        // // timelock should be empty
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(0)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(depositedTokens)
+    })
+
+    it('Release soon should not work', async () => {
+        const { psp22: psp22Container, timelock: timelockContainer, beneficiary } = await setup()
+        const { contract: psp22, query: psp22Query } = psp22Container
+        const { contract: timelock } = timelockContainer
+
+        let depositedTokens = 1000
+        // deposit tokens to the contract
+        await expect(fromSigner(psp22, beneficiary.address).tx.transfer(timelock.address, depositedTokens, [])).to.eventually.be.fulfilled
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(depositedTokens)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(0)
+
+        // release the tokens early
+        await expect(fromSigner(timelock, beneficiary.address).tx.release())
+
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(depositedTokens)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(0)
+    })
+
+    it('Release without deposit should not work', async () => {
+        const { psp22: psp22Container, timelock: timelockContainer, beneficiary, releaseTime } = await setup()
+        const { query: psp22Query } = psp22Container
+        const { contract: timelock } = timelockContainer
+
+        let tokens = 1000
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(0)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(tokens)
+        await api.tx.timestamp.set(releaseTime).signAndSend(beneficiary.pair)
+
+        // release the tokens
+        await expect(fromSigner(timelock, beneficiary.address).tx.release()).to.eventually.be.rejected
+
+        // // timelock should be empty
+        await expect(psp22Query.balanceOf(timelock.address)).to.have.output(0)
+        await expect(psp22Query.balanceOf(beneficiary.address)).to.have.output(tokens)
+    })
+
+})
