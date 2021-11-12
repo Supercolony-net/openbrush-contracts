dc152d4a976fa676d0a143b6ed9bd48da9de5f30
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-11-12 19:46:50 +0100

    comments for flashmint

--- contracts/token/psp22/src/extensions/flashmint.rs
+++ contracts/token/psp22/src/extensions/flashmint.rs
@@ -19,6 +19,8 @@ use ink_prelude::{
 pub trait PSP22FlashMint: PSP22 {
     const RETURN_VALUE: [u8; 32] = brush::blake2b_256!("PSP3156FlashBorrower.onFlashLoan");
 
+    /// Maximum amount of `token` available to mint
+    /// Bounded by the max value of Balance (u128)
     #[ink(message)]
     fn max_flashloan(&mut self, token: AccountId) -> Balance {
         if token == Self::env().account_id() {
@@ -28,6 +30,9 @@ pub trait PSP22FlashMint: PSP22 {
         }
     }
 
+    /// Fee for borrowing `amount` of the `token`
+    /// 
+    /// Returns `Wrong token` error if the `token` account id is not this token
     #[ink(message)]
     fn flash_fee(&mut self, token: AccountId, amount: Balance) -> Result<Balance, PSP22Error> {
         if token != Self::env().account_id() {
@@ -36,6 +41,14 @@ pub trait PSP22FlashMint: PSP22 {
         Ok(self.get_fee(amount))
     }
 
+    /// Mints `amount` of `token` to `receiver_account` and performs the flashloan
+    /// `amount` is then burned along with the fee for the flashloan
+    /// 
+    /// `receiver_account` must implement `PSP3156FlashBorrower`
+    /// 
+    /// Returns `Invalid return value` error if the `receiver_account` returns incorrect bytes
+    /// Returns `Allowance does not allow refund` error if the contract does not have 
+    /// enough allowance to transfer borrowed amount and fees from `receiver_account` 
     #[ink(message)]
     fn flashloan(
         &mut self,
@@ -59,10 +72,12 @@ pub trait PSP22FlashMint: PSP22 {
         self._burn(receiver_account, amount + fee)
     }
 
+    /// Helper function to get fee for borrowing `amount` of token
     fn get_fee(&mut self, _amount: Balance) -> Balance {
         0
     }
 
+    /// Helper function which calls `on_flashloan` on `receiver_account`
     fn on_flashloan(
         &mut self,
         receiver_account: AccountId,
