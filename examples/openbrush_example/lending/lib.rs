74253a450198abee6feef76817b9d12574f7de86
kriko.eth <43150707+coreggon11@users.noreply.github.com>
2021-11-25 15:48:54 +0100

    grammar

--- examples/openbrush_example/lending/lib.rs
+++ examples/openbrush_example/lending/lib.rs
@@ -2,19 +2,19 @@
 
 /// This will be a simple lending contract where users can:
 ///
-/// 1. Lend tokens allowed by the smart contract.
+/// 1. Lend tokens accepted by the smart contract.
 /// The allowance and disallowance of tokens is done by the accounts which have an admin role
 /// Upon lending, the user gets a PSP-22 token representing their share of the current liquidity pool
 ///
 /// 2. Borrow tokens from the smart contract by depositing collateral tokens.
-/// The tokens which can be deposited as a collateral are allowed and disallowed by the accounts with admin role
+/// The tokens which can be deposited as collateral are allowed and disallowed by the accounts with admin role
 /// Upon borrowing user gets a PSP-721 token representing info about their loan (how much assets were borrowed,
-/// when did they borrow, what asset was borrowed, what asset was used as a collateral, how much collateral assets
+/// when did they borrow, what asset was borrowed, what asset was used as collateral, how much collateral assets
 /// were deposited, the liquidation price of the loan and if it was liquidated or not)
 ///
 /// 3. Repay their loan by depositing the borrowed amount of borrowed assets along with interest.
 /// The contract determines how much a user needs to deposit and how much collateral they get back by an NFT token
-/// which the user gets upon borrowing the assets. User is also able to repay a portion of the loan, but will only get
+/// which the user gets upon borrowing the assets. The user is also able to repay a portion of the loan, but will only get
 /// a portion of their collateral assets back, while the liquidation price will stay the same
 ///
 /// 4. Swap their collateral tokens to repay the borrowed amount of borrowed assets with interest.
