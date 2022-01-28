55e953145f1b371cbf0b951ecded644dde9e6065
yarikbratashchuk <yarik.bratashchuk@gmail.com>
2022-01-28 11:16:52 +0200

    Fix eslint indent

--- tests/psp34/extensions/metadata.tests.ts
+++ tests/psp34/extensions/metadata.tests.ts
@@ -1,14 +1,14 @@
 import { expect, setupContract } from '../../helpers'
 
 describe('MY_PSP34_METADATA', () => {
-    async function setup() {
-        return setupContract('my_psp34_metadata', 'new', 1, 'Non Fungible Token', 'NFT')
-    }
+  async function setup() {
+    return setupContract('my_psp34_metadata', 'new', 1, 'Non Fungible Token', 'NFT')
+  }
 
-    it('Metadata works', async () => {
-        const { query } = await setup()
+  it('Metadata works', async () => {
+    const { query } = await setup()
 
-        // TODO: add test for getting attributes using get_attribute
-    })
+    // TODO: add test for getting attributes using get_attribute
+  })
 
 })
