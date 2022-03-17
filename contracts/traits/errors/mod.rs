e6eb782cc019f744c47539260e603fc84f548c4a
SupremaLex <georglutsenko@gmail.com>
2022-03-17 13:23:46 +0200

    Removed ProxyError. Use OwnableError instead

--- contracts/traits/errors/mod.rs
+++ contracts/traits/errors/mod.rs
@@ -3,7 +3,6 @@ mod flashloan;
 mod ownable;
 mod pausable;
 mod payment_splitter;
-mod proxy;
 mod psp1155;
 mod psp22;
 mod psp34;
@@ -18,7 +17,6 @@ pub use flashloan::{
 pub use ownable::OwnableError;
 pub use pausable::PausableError;
 pub use payment_splitter::PaymentSplitterError;
-pub use proxy::ProxyError;
 pub use psp1155::{
     PSP1155Error,
     PSP1155ReceiverError,
