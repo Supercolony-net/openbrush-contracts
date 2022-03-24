f4b40fdb20720a06a394b6c66557dfff7946a820
Nikita Samardak <nikita.samardak@playtika.com>
2022-03-24 19:52:42 +0200

    enumerable after approved transfer works

--- contracts/tests/psp34_enumerable.rs
+++ contracts/tests/psp34_enumerable.rs
@@ -115,4 +115,28 @@ mod psp34_enumerable {
         //alice does not own token
         assert_eq!(nft.owners_token_by_index(accounts.alice, 0u128), Err(PSP34Error::TokenNotExists));
     }
+    #[ink::test]
+    fn enumerable_after_approved_transfer_works() {
+        let accounts = accounts();
+        // Create a new contract instance.
+        let mut nft = PSP34Struct::new();
+        assert!(nft._mint_to(accounts.alice, Id::U8(1u8)).is_ok());
+        assert!(nft._mint_to(accounts.alice, Id::U8(2u8)).is_ok());
+        // Token Id 1 is owned by Alice.
+        assert_eq!(nft.owner_of(Id::U8(1u8)), Some(accounts.alice));
+        // Approve token Id 1 transfer for Bob on behalf of Alice.
+        assert!(nft.approve(accounts.bob, Some(Id::U8(1u8)), true).is_ok());
+        // Get contract address.
+        change_caller(accounts.bob);
+        // Bob transfers token Id 1 from Alice to Eve.
+        assert!(nft.transfer(accounts.eve, Id::U8(1u8), vec![]).is_ok());
+        //eve owns token id 1
+        assert_eq!(nft.owners_token_by_index(accounts.eve, 0u128), Ok(Id::U8(1u8)));
+        //alice owns token id 2
+        assert_eq!(nft.owners_token_by_index(accounts.alice, 0u128), Ok(Id::U8(2u8)));
+        //bob does not owns any tokens
+        assert_eq!(nft.owners_token_by_index(accounts.bob, 0u128), Err(PSP34Error::TokenNotExists));
+        //index 1 points to token with id 2
+        assert_eq!(nft.token_by_index(1u128), Ok(Id::U8(2u8)));
+    }
 }
