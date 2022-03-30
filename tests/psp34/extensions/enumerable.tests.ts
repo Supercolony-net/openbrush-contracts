a6077d5b3fbd43ad3f79e57ca74c843f3bfd9d87
iron.man@supercolony.net <iron.man@supercolony.net>
2022-03-30 19:21:53 +0300

    complete integration tests for psp34_enumerable

--- tests/psp34/extensions/enumerable.tests.ts
+++ tests/psp34/extensions/enumerable.tests.ts
@@ -46,7 +46,7 @@ describe('MY_PSP34_ENUMERABLE', () => {
     let psp34_id2 = {
       "u8" : 2,
     };
-    
+
     await contract.tx.mint(alice.address, psp34_id1)
     await contract.tx.mint(alice.address, psp34_id2)
 
@@ -57,23 +57,35 @@ describe('MY_PSP34_ENUMERABLE', () => {
     expect(result((await query.ownersTokenByIndex(alice.address,1)).output?.toString()).ok.u8).equal(2);
   })
 
-//   it('Mint existing should fail', async () => {
-//     const {
-//       contract,
-//       accounts: [alice],
-//       defaultSigner: sender,
-//       query
-//     } = await setup()
+  it('Enumerable works after burn', async () => {
+    const {
+      contract,
+      defaultSigner: sender,
+      accounts: [alice],
+      query
+    } = await setup()
 
-//     await expect(contract.tx.mint(sender.address, 0)).to.eventually.be.fulfilled
-//     await expect(query.balanceOf(sender.address)).to.have.output(1)
-//     await expect(query.balanceOf(alice.address)).to.have.output(0)
+    await expect(contract.tx.ownersTokenByIndex(sender.address,0)).to.eventually.be.rejected
+    await expect(contract.tx.ownersTokenByIndex(alice.address,0)).to.eventually.be.rejected
 
-//     await expect(contract.tx.mint(sender.address, 0)).to.eventually.be.rejected
-//     await expect(contract.tx.mint(alice.address, 0)).to.eventually.be.rejected
+    let psp34_id1 = {
+      "u8" : 1,
+    };
+    let psp34_id2 = {
+      "u8" : 2,
+    };
+    
+    await contract.tx.mint(alice.address, psp34_id1)
+    await contract.tx.mint(alice.address, psp34_id2)
 
-//     await expect(query.balanceOf(sender.address)).to.have.output(1)
-//     await expect(query.balanceOf(alice.address)).to.have.output(0)
-//   })
+    expect(result((await query.tokenByIndex(0)).output?.toString()).ok.u8).equal(1);
+    expect(result((await query.tokenByIndex(1)).output?.toString()).ok.u8).equal(2);
+
+    await contract.tx.burn(alice.address, psp34_id2)
+
+    await expect(contract.tx.ownersTokenByIndex(alice.address,0)).to.eventually.be.fulfilled
+    await expect(contract.tx.ownersTokenByIndex(alice.address,1)).to.eventually.be.rejected
+
+  })
 
 })
