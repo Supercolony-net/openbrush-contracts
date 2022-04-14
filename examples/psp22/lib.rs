3d812478f104ccd1a331770e033a6bb4b8ff34de
iron.man@supercolony.net <iron.man@supercolony.net>
2022-04-14 23:16:16 +0300

    add create asset pallet message in psp22. completed. tested

--- examples/psp22/lib.rs
+++ examples/psp22/lib.rs
@@ -1,56 +1,193 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![feature(min_specialization)]
 
-#[brush::contract]
-pub mod my_psp22 {
-    use brush::contracts::psp22::*;
-    use ink_prelude::string::String;
-    use ink_storage::traits::SpreadAllocate;
+use ink_env::Environment;
+use ink_lang as ink;
 
+/// This is an example of how an ink! contract may call the Substrate
+/// runtime function `RandomnessCollectiveFlip::random_seed`. See the
+/// file `runtime/chain-extension-example.rs` for that implementation.
+///
+/// Here we define the operations to interact with the Substrate runtime.
+#[ink::chain_extension]
+pub trait FetchRandom {
+    type ErrorCode = RandomReadErr;
+
+    /// Note: this gives the operation a corresponding `func_id` (1101 in this case),
+    /// and the chain-side chain extension will get the `func_id` to do further operations.
+    #[ink(extension = 1101, returns_result = false)]
+    fn fetch_random(subject: [u8; 32]) -> [u8; 32];
+
+    #[ink(extension = 1102, returns_result = false)]
+    fn create(subject: CreateAsset) -> [u8; 32];
+
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum OriginType{
+	Caller, 
+	Address
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+pub struct CreateAsset{
+	origin_type: OriginType,
+	asset_id : u32, 
+	admin_address : [u8; 32], 
+	min_balance : u128
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum RandomReadErr {
+    FailGetRandomSource,
+}
+
+impl ink_env::chain_extension::FromStatusCode for RandomReadErr {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::FailGetRandomSource),
+            _ => panic!("encountered unknown status code"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CustomEnvironment {}
+
+impl Environment for CustomEnvironment {
+    const MAX_EVENT_TOPICS: usize =
+        <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+
+    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
+
+    type ChainExtension = FetchRandom;
+}
+
+#[ink::contract(env = crate::CustomEnvironment)]
+mod rand_extension {
+    use super::RandomReadErr;
+    use crate::CreateAsset;
+    use crate::OriginType;
+
+    /// Defines the storage of our contract.
+    ///
+    /// Here we store the random seed fetched from the chain.
     #[ink(storage)]
-    #[derive(Default, SpreadAllocate, PSP22Storage)]
-    pub struct MyPSP22 {
-        #[PSP22StorageField]
-        psp22: PSP22Data,
-        // fields for hater logic
-        hated_account: AccountId,
+    pub struct RandExtension {
+        /// Stores a single `bool` value on the storage.
+        value: [u8; 32],
     }
 
-    impl PSP22Transfer for MyPSP22{
-        // Let's override method to reject transactions to bad account
-        fn _before_token_transfer(
-            &mut self,
-            _from: Option<&AccountId>,
-            to: Option<&AccountId>,
-            _amount: &Balance,
-        ) -> Result<(), PSP22Error> {
-            if to == Some(&self.hated_account) {
-                return Err(PSP22Error::Custom(String::from("I hate this account!")))
-            }
+    #[ink(event)]
+    pub struct RandomUpdated {
+        #[ink(topic)]
+        new: [u8; 32],
+    }
+
+    impl RandExtension {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(init_value: [u8; 32]) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors may delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// Seed a random value by passing some known argument `subject` to the runtime's
+        /// random source. Then, update the current `value` stored in this contract with the
+        /// new random value.
+        #[ink(message)]
+        pub fn update(&mut self, subject: [u8; 32]) -> Result<(), RandomReadErr> {
+            // Get the on-chain random seed
+            let new_random = self.env().extension().fetch_random(subject)?;
+            self.value = new_random;
+            // Emit the `RandomUpdated` event when the random seed
+            // is successfully fetched.
+            self.env().emit_event(RandomUpdated { new: new_random });
             Ok(())
         }
+
+        #[ink(message)]
+        pub fn create_pallet_asset(&mut self, 
+            origin_type: OriginType,
+            asset_id : u32, 
+            admin_address : [u8; 32], 
+            min_balance : u128) -> Result<(), RandomReadErr> {
+            // Get the on-chain random seed
+            let input = CreateAsset{origin_type, asset_id, admin_address, min_balance};
+            let new_random = self.env().extension().create(input)?;
+            // self.value = new_random;
+            // Emit the `RandomUpdated` event when the random seed
+            // is successfully fetched.
+            // self.env().emit_event(RandomUpdated { new: new_random });
+            Ok(())
+        }
+
+        /// Simply returns the current value.
+        #[ink(message)]
+        pub fn get(&self) -> [u8; 32] {
+            self.value
+        }
     }
 
-    impl PSP22 for MyPSP22 {}
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+        use ink_lang as ink;
 
-    impl MyPSP22 {
-        #[ink(constructor)]
-        pub fn new(total_supply: Balance) -> Self {
-            ink_lang::codegen::initialize_contract(|instance: &mut MyPSP22| {
-                instance
-                    ._mint(instance.env().caller(), total_supply)
-                    .expect("Should mint");
-            })
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let rand_extension = RandExtension::default();
+            assert_eq!(rand_extension.get(), [0; 32]);
         }
 
-        #[ink(message)]
-        pub fn set_hated_account(&mut self, hated: AccountId) {
-            self.hated_account = hated;
-        }
+        #[ink::test]
+        fn chain_extension_works() {
+            // given
+            struct MockedExtension;
+            impl ink_env::test::ChainExtension for MockedExtension {
+                /// The static function id of the chain extension.
+                fn func_id(&self) -> u32 {
+                    1101
+                }
 
-        #[ink(message)]
-        pub fn get_hated_account(&self) -> AccountId {
-            self.hated_account.clone()
+                /// The chain extension is called with the given input.
+                ///
+                /// Returns an error code and may fill the `output` buffer with a
+                /// SCALE encoded result. The error code is taken from the
+                /// `ink_env::chain_extension::FromStatusCode` implementation for
+                /// `RandomReadErr`.
+                fn call(&mut self, _input: &[u8], output: &mut Vec<u8>) -> u32 {
+                    let ret: [u8; 32] = [1; 32];
+                    scale::Encode::encode_to(&ret, output);
+                    0
+                }
+            }
+            ink_env::test::register_chain_extension(MockedExtension);
+            let mut rand_extension = RandExtension::default();
+            assert_eq!(rand_extension.get(), [0; 32]);
+
+            // when
+            rand_extension.update([0_u8; 32]).expect("update must work");
+
+            // then
+            assert_eq!(rand_extension.get(), [1; 32]);
         }
     }
 }
