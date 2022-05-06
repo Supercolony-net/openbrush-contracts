f108fb2db16135db4d6f1bf2100e6d4c774f7eda
iron.man@supercolony.net <iron.man@supercolony.net>
2022-05-06 10:16:40 +0300

    complete mintable integration tests for psp22

--- tests/psp22_assets/extensions/mintable.tests.ts
+++ tests/psp22_assets/extensions/mintable.tests.ts
@@ -26,29 +26,29 @@ describe('MY_PSP22_ASSET_MINTABLE', () => {
         await expect(query.balanceOf(sender.address)).to.have.output(0)
     })
 
-    // it('Minting requested amount', async () => {
-    //     const { contract, query, accounts: [alice],  } = await setup()
+    it('Minting requested amount', async () => {
+        const { contract, query, accounts: [alice],  } = await setup()
 
-    //     // Arrange - Ensure receiver balance is 0
-    //     await expect(query.balanceOf(alice.address, bnArg(0))).to.have.output(0)
+        // Arrange - Ensure receiver balance is 0
+        await expect(query.balanceOf(alice.address, bnArg(0))).to.have.output(0)
 
-    //     // Act - Sender mint a token
-    //     // await expect(contract.tx.mint(alice.address, 1)).to.eventually.be.fulfilled
+        // Act - Sender mint a token
+        await expect(contract.tx.mint(alice.address, 1)).to.eventually.be.fulfilled
 
-    //     // Assert - Sender balance is now 1
-    //     // await expect(query.balanceOf(alice.address, bnArg(0))).to.have.output(1)
-    // })
+        // Assert - Sender balance is now 1
+        await expect(query.balanceOf(alice.address, bnArg(0))).to.have.output(1)
+    })
 
-    // it('Increases total supply after minting', async () => {
-    //     const { contract, query, defaultSigner: sender } = await setup()
+    it('Increases total supply after minting', async () => {
+        const { contract, query, defaultSigner: sender } = await setup()
 
-    //     // Arrange - Ensure initial supply is correct
-    //     await expect(query.totalSupply()).to.have.output(0)
+        // Arrange - Ensure initial supply is correct
+        await expect(query.totalSupply()).to.have.output(0)
 
-    //     // Act - Sender mint a token
-    //     await expect(contract.tx.mint(sender.address, 1)).to.eventually.be.fulfilled
+        // Act - Sender mint a token
+        await expect(contract.tx.mint(sender.address, 1)).to.eventually.be.fulfilled
 
-    //     // Assert - Sender balance is now 1
-    //     await expect(query.totalSupply()).to.have.output(1)
-    // })
+        // Assert - Sender balance is now 1
+        await expect(query.totalSupply()).to.have.output(1)
+    })
 })
