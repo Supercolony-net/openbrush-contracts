b65992ad03fd4d7d8792b8e835d048bb73d6a431
Artemka374 <fomiuk.artemka123@gmail.com>
2022-06-11 13:40:33 +0300

    fix tests

--- tests/e2e/psp35/extensions/enumerable.tests.ts
+++ tests/e2e/psp35/extensions/enumerable.tests.ts
@@ -1,9 +1,11 @@
-import { bnArg, expect, setupContract } from '../../helpers'
-import { bnFromHex } from '@polkadot/util'
-import BN from 'bn.js'
+import { expect, setupContract } from '../../helpers'
 
 interface Result {
-  ok: string;
+  ok: Ok;
+}
+
+interface Ok {
+  u8: number;
 }
 
 describe('MY_PSP35_ENUMERABLE', () => {
@@ -38,18 +40,18 @@ describe('MY_PSP35_ENUMERABLE', () => {
     await expect(contract.tx.ownersTokenByIndex(sender.address, 0)).to.eventually.be.rejected
     await expect(contract.tx.ownersTokenByIndex(alice.address, 0)).to.eventually.be.rejected
 
-    const token1 = bnArg(1)
-    const token2 = bnArg(2)
+    const token1 = {'u8': 1 }
+    const token2 = {'u8': 2 }
     const amount1 = 1
     const amount2 = 20
 
     await expect(contract.tx.mint(alice.address, [[token1, amount1], [token2, amount2]])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.tokenByIndex(0)).output?.toString()).ok)).equal(new BN(token1))
-    expect(bnFromHex(result((await query.tokenByIndex(1)).output?.toString()).ok)).equal(new BN(token2))
+    expect(result((await query.tokenByIndex(0)).output?.toString()).ok.u8).equal(1)
+    expect(result((await query.tokenByIndex(1)).output?.toString()).ok.u8).equal(2)
 
-    expect(bnFromHex(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok)).equal(new BN(token1))
-    expect(bnFromHex(result((await query.ownersTokenByIndex(alice.address, 1)).output?.toString()).ok)).equal(new BN(token2))
+    expect(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok.u8).equal(1)
+    expect(result((await query.ownersTokenByIndex(alice.address, 1)).output?.toString()).ok.u8).equal(2)
   })
 
   it('Enumerable works after burn', async () => {
@@ -63,19 +65,19 @@ describe('MY_PSP35_ENUMERABLE', () => {
     await expect(contract.tx.ownersTokenByIndex(sender.address, 0)).to.eventually.be.rejected
     await expect(contract.tx.ownersTokenByIndex(alice.address, 0)).to.eventually.be.rejected
 
-    const token1 = bnArg(1)
-    const token2 = bnArg(2)
+    const token1 = {'u8': 1}
+    const token2 = {'u8': 2}
     const amount1 = 1
     const amount2 = 20
 
     await expect(contract.tx.mint(alice.address, [[token1, amount1], [token2, amount2]])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.tokenByIndex(0)).output?.toString()).ok)).equal(new BN(token1))
-    expect(bnFromHex(result((await query.tokenByIndex(1)).output?.toString()).ok)).equal(new BN(token2))
+    expect(result((await query.tokenByIndex(0)).output?.toString()).ok.u8).equal(1)
+    expect(result((await query.tokenByIndex(1)).output?.toString()).ok.u8).equal(2)
 
     await expect(contract.tx.burn(alice.address, [[token2, amount2]])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok)).equal(new BN(token1))
+    expect(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok.u8).equal(1)
     await expect(contract.query.ownersTokenByIndex(alice.address, 1)).to.eventually.be.rejected
   })
 
@@ -90,22 +92,22 @@ describe('MY_PSP35_ENUMERABLE', () => {
     await expect(contract.tx.ownersTokenByIndex(sender.address, 0)).to.eventually.be.rejected
     await expect(contract.tx.ownersTokenByIndex(alice.address, 0)).to.eventually.be.rejected
 
-    const token1 = bnArg(1)
-    const token2 = bnArg(2)
+    const token1 = {'u8': 1}
+    const token2 = {'u8': 2}
     const amount1 = 1
     const amount2 = 20
 
     await expect(contract.tx.mint(sender.address, [[token1, amount1], [token2, amount2]])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok)).equal(new BN(token1))
+    expect(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok.u8).equal(1)
     await expect(contract.tx.transfer(alice.address, token1, amount1, [])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok)).equal(new BN(token1))
-    expect(bnFromHex(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok)).equal(new BN(token2))
+    expect(result((await query.ownersTokenByIndex(alice.address, 0)).output?.toString()).ok.u8).equal(1)
+    expect(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok.u8).equal(2)
 
     await expect(contract.tx.transfer(alice.address, token2, 10, [])).to.eventually.be.fulfilled
 
-    expect(bnFromHex(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok)).equal(new BN(token2))
-    expect(bnFromHex(result((await query.ownersTokenByIndex(alice.address, 1)).output?.toString()).ok)).equal(new BN(token2))
+    expect(result((await query.ownersTokenByIndex(sender.address, 0)).output?.toString()).ok.u8).equal(2)
+    expect(result((await query.ownersTokenByIndex(alice.address, 1)).output?.toString()).ok.u8).equal(2)
   })
 })
