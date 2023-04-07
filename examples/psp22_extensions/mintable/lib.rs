6a36052ef9d7f8ff0901e51ac8fb3a6000f773af
Varex Silver <limposfeed@gmail.com>
2023-04-07 20:32:04 +0300

    Add PSP22-Metadata, PSP22-Mintable, PSP22-Pausable tests

--- examples/psp22_extensions/mintable/lib.rs
+++ examples/psp22_extensions/mintable/lib.rs
@@ -16,6 +16,7 @@ pub mod my_psp22_mintable {
     }
 
     impl PSP22 for Contract {}
+
     impl PSP22Mintable for Contract {}
 
     impl Contract {
@@ -31,6 +32,101 @@ pub mod my_psp22_mintable {
         #[ink(message)]
         pub fn mint_to(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
             self.mint(account, amount)
+        }
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    pub mod tests {
+        use openbrush::contracts::psp22::psp22_external::PSP22;
+        use openbrush::contracts::psp22::extensions::mintable::psp22mintable_external::PSP22Mintable;
+
+        #[rustfmt::skip]
+        use super::*;
+        #[rustfmt::skip]
+        use ink_e2e::{build_message, PolkadotConfig};
+
+        use test_helpers::{
+            address_of,
+            balance_of
+        };
+
+        type E2EResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+        #[ink_e2e::test]
+        async fn assigns_initial_balance(client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = ContractRef::new(1000);
+            let address = client.instantiate("my_psp22_mintable", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            assert!(matches!(balance_of!(client, address, alice), 1000));
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn minting_requested_amount(client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = ContractRef::new(1000);
+            let address = client.instantiate("my_psp22_mintable", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            assert!(matches!(balance_of!(client, address, bob), 0), "Bob's balance should be 0");
+
+            let mint_tx = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.mint(address_of!(bob), 1000));
+                client.call(&ink_e2e::alice(), _msg, 0, None)
+                    .await
+                    .expect("transfer failed")
+            };
+
+            assert!(matches!(mint_tx.return_value(), Ok(())), "Minting should be successful");
+
+            assert!(matches!(balance_of!(client, address, bob), 1000), "Bob's balance should be 1000");
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn increases_total_supply_after_minting(client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = ContractRef::new(0);
+            let address = client.instantiate("my_psp22_mintable", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let total_supply = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.total_supply());
+                client.call_dry_run(&ink_e2e::alice(), &_msg, 0, None)
+                    .await
+            }.return_value();
+
+            assert!(matches!(total_supply, 0), "Total supply should be 0");
+
+            let mint_tx = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.mint(address_of!(bob), 1000));
+                client.call(&ink_e2e::alice(), _msg, 0, None)
+                    .await
+                    .expect("transfer failed")
+            };
+
+            assert!(matches!(mint_tx.return_value(), Ok(())), "Minting should be successful");
+
+            let total_supply = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.total_supply());
+                client.call_dry_run(&ink_e2e::alice(), &_msg, 0, None)
+                    .await
+            }.return_value();
+
+            assert!(matches!(total_supply, 1000), "Total supply should be 1000");
+
+            Ok(())
         }
     }
 }
