7605505c727851cee5742277ab6028564ceed7d0
Varex Silver <limposfeed@gmail.com>
2023-05-03 13:39:20 +0300

    Update CI

---
+++ e2e-tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide at least one glob pattern as an argument."
+  exit 1
+fi
+
+IGNORED_DIRS=("./examples/reentrancy_guard/**" "./examples/test_helpers/**")
+
+contains_element() {
+  local element
+  for element in "${@:2}"; do
+    [[ "$element" == "$1" ]] && return 0
+  done
+  return 1
+}
+
+process_directory() {
+  local dir=$1
+
+  if contains_element "$(basename "$dir")" "${IGNORED_DIRS[@]}"; then
+    return
+  fi
+
+  if [ -f "${dir}Cargo.toml" ]; then
+    cd "$dir" || exit
+
+    echo "Building contract in $dir"
+    cargo contract build
+
+    echo "Running e2e-tests in $dir"
+    cargo test --features e2e-tests
+
+    cd - || exit
+  fi
+}
+
+for pattern in "$@"; do
+  for dir in $pattern/*/; do
+    process_directory "$dir"
+  done
+done
